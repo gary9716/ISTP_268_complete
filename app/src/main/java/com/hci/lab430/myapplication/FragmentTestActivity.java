ee20a51fb217644a725d715a2946465dd8eef357
lab430 <lab430@lab430deMacBook-Pro.local>
2016-08-04 18:00:31 +0800

    remove displayFragment

--- app/src/main/java/com/hci/lab430/myapplication/FragmentTestActivity.java
+++ app/src/main/java/com/hci/lab430/myapplication/FragmentTestActivity.java
@@ -7,8 +7,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.hci.lab430.myapplication.fragment.LogFragment;
 import com.hci.lab430.myapplication.fragment.TestFragment1;
-import com.hci.lab430.myapplication.fragment.TestFragment2;
 
 /**
  * Created by lab430 on 16/8/1.
@@ -17,7 +17,6 @@ public class FragmentTestActivity extends AppCompatActivity implements View.OnCl
 
     int fragmentContainerId;
     FragmentManager fragmentManager;
-    final int mode = 2;
     Fragment[] fragments;
 
     @Override
@@ -33,8 +32,9 @@ public class FragmentTestActivity extends AppCompatActivity implements View.OnCl
 
         fragments = new Fragment[2];
         fragments[0] = TestFragment1.newInstance("Fragment 1 pre-allocated");
-        fragments[1] = TestFragment2.newInstance(R.drawable.pokemon);
-
+        ((LogFragment)fragments[0]).actualName = "F1";
+        fragments[1] = TestFragment1.newInstance("Fragment 2 pre-allocated");
+        ((LogFragment)fragments[1]).actualName = "F2";
     }
 
     @Override
@@ -46,20 +46,12 @@ public class FragmentTestActivity extends AppCompatActivity implements View.OnCl
     public void onClick(View view) {
         int viewId = view.getId();
         if(viewId == R.id.fragment_1) {
-            if(mode == 1) {
-                replaceWithTheFragment(fragments[0]);
-            }
-            else if(mode == 2) {
-                displayFragment(fragments[0]);
-            }
+            replaceWithTheFragment(fragments[0]);
+
         }
         else if(viewId == R.id.fragment_2) {
-            if(mode == 1) {
-                replaceWithTheFragment(fragments[1]);
-            }
-            else if(mode == 2) {
-                displayFragment(fragments[1]);
-            }
+            replaceWithTheFragment(fragments[1]);
+
         }
         else if(viewId == R.id.fragment_2_remove) {
             removeTheFragment(fragments[1]);
@@ -77,23 +69,6 @@ public class FragmentTestActivity extends AppCompatActivity implements View.OnCl
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(fragmentContainerId, fragment);
         transaction.addToBackStack(null); //let back button be able to reverse this commitment
-        transaction.commit();
-    }
-
-    private void displayFragment(Fragment fragment) {
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        if(!fragment.isAdded()) {
-            transaction.add(fragmentContainerId, fragment);
-        }
-        else {
-            for(int i = 0;i < fragments.length; i++) {
-                if(fragments[i].isVisible()) {
-                    transaction.hide(fragments[i]);
-                }
-            }
-            transaction.show(fragment);
-        }
-
         transaction.commit();
     }
 
